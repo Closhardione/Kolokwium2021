93b32de9a37148f56d9747bbcd3ea58ead483ed7
Olek <olekston@gmail.com>
2023-05-11 20:48:41 +0200

    4,5 zad

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Country test = null;
+        try {
+            test = Country.fromCsv("Australia");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (CountryNotFoundException e) {
+            e.getMessage();
+        }
+        System.out.println(test);
     }
 }
